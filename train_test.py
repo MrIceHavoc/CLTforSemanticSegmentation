f71fed32722bc9ae7f3022b7c4aaa49a759b584b
MrIceHavoc <afonso.fernandes100@gmail.com>
2022-12-03 02:38:08 +0900

    Implemented train pipeline

--- train_test.py
+++ train_test.py
@@ -13,20 +13,57 @@ from mmseg.datasets import build_dataset
 import torch
 
 DATASETS_DIR = '/mmsegmentation/data/'
+VOC_DATASET = os.path.join(DATASETS_DIR, 'VOCdevkit/VOC2012/')
 
 def main(checkpoint, config_file, dataset, mode):
     if mode == 'train':
         cfg = mmcv.Config.fromfile(config_file) 
+        cfg.dataset_type = 'voc'
+        cfg.dataset_root = dataset
         cfg.model.decode_head.num_classes = 21
         cfg.model.auxiliary_head.num_classes = 21
+        
+        cfg.data.train.type = cfg.dataset_type
+        cfg.data.train.data_root = cfg.data_root
+        cfg.data.train.img_dir = os.path.join(dataset, 'JPEGImages/')
+        cfg.data.train.ann_dir = os.path.join(dataset, 'Annotations/')
+        cfg.data.train.pipeline = cfg.train_pipeline
+        cfg.data.train.split = os.path.join(dataset, 'ImageSets/Segmentation/train.txt')
+        cfg.load_from = checkpoint
+
+        cfg.work_dir = './work_dirs/clt_segmentation'
+        cfg.seed = 42
         cfg.device = get_device()
-        model = build_segmentor(cfg.model)
+
         datasets = [build_dataset(cfg.data.train)]
+        model = build_segmentor(cfg.model)
         model.CLASSES = datasets[0].CLASSES
         mmcv.mkdir_or_exist(os.path.abspath(cfg.work_dir))
         train_segmentor(model, datasets, cfg, distributed=False, validate=True, meta=dict())
     elif mode == 'test':
         cfg = mmcv.Config.fromfile(config_file)
+        cfg.dataset_type = 'voc'
+        cfg.dataset_root = dataset
+        cfg.model.decode_head.num_classes = 21
+        cfg.model.auxiliary_head.num_classes = 21
+
+        cfg.data.test.type = cfg.dataset_type
+        cfg.data.test.data_root = cfg.data_root
+        cfg.data.test.img_dir = os.path.join(dataset, 'JPEGImages/')
+        cfg.data.test.ann_dir = os.path.join(dataset, 'Annotations/')
+        cfg.data.test.pipeline = cfg.test_pipeline
+        cfg.data.test.split = os.path.join(dataset, 'ImageSets/Segmentation/val.txt')
+        cfg.load_from = checkpoint
+
+        model = build_segmentor(cfg.model)
+        cfg.work_dir = './work_dirs/clt_segmentation'
+        cfg.seed = 42
+        cfg.device = get_device()
+
+        datasets = [build_dataset(cfg.data.train)]
+        model = build_segmentor(cfg.model)
+        model.CLASSES = datasets[0].CLASSES
+        mmcv.mkdir_or_exist(os.path.abspath(cfg.work_dir))
     elif mode == 'inference':
         model = init_segmentor(config_file, checkpoint)
         img_dir = 'JPEGImages/'
@@ -38,9 +75,9 @@ def main(checkpoint, config_file, dataset, mode):
 if __name__ == "__main__":
     cwd = os.getcwd()
     parser = argparse.ArgumentParser(prog='CLT', description="Train/Test Contrastive Learning Transformer for Semantic Segmentation.")
-    parser.add_argument('--model', default=cwd + '/segmenter_vit-t_mask_8x1_512x512_160k_ade20k_20220105_151706-ffcf7509.pth')
+    parser.add_argument('--model', default=os.path.join(cwd, '/segmenter_vit-t_mask_8x1_512x512_160k_ade20k_20220105_151706-ffcf7509.pth'))
     parser.add_argument('--config', default=cwd + '/config.py')
-    parser.add_argument('--dataset', type=pathlib.Path, default=cwd + DATASETS_DIR + 'VOCdevkit/VOC2012/')
+    parser.add_argument('--dataset', type=pathlib.Path, default=os.path.join(cwd, VOC_DATASET))
     parser.add_argument('--mode', choices=['train', 'test', 'inference'], default='train')
     args = vars(parser.parse_args())
     main(args['model'], args['config'], args['dataset'], args['mode'])
