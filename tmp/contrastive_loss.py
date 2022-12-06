70a86af282662dd9930bdd0ca3d0ccb5cd88bbd2
MrIceHavoc <afonso.fernandes100@gmail.com>
2022-12-06 23:08:04 +0900

    Implemented contrastive loss

--- tmp/contrastive_loss.py
+++ tmp/contrastive_loss.py
@@ -1,32 +1,42 @@
 import torch
 import torch.nn as nn
+import pytorch_lightning as pl
 
 from ..builder import LOSSES
-from .utils import weighted_loss
-
-@weighted_loss
-def contrastive_loss(pred, target):
-    assert pred.size() == target.size() and target.numel() > 0
-    loss = torch.abs(pred - target)
-    return loss
 
 @LOSSES.register_module
-class ContrastiveLoss(nn.Module):
+class ContrastiveLoss(pl.LightningModule):
 
-    def __init__(self, reduction='mean', loss_weight=1.0):
-        super(MyLoss, self).__init__()
-        self.reduction = reduction
-        self.loss_weight = loss_weight
+    def __init__(self, loss_weight=1.0):
+        super().__init__()
+        assert loss_weight > 0.0, 'The temperature parameter must be a positive float!'
+        self.temperature = loss_weight
 
-    def forward(self,
-                pred,
-                target,
-                weight=None,
-                avg_factor=None,
-                reduction_override=None):
-        assert reduction_override in (None, 'none', 'mean', 'sum')
-        reduction = (
-            reduction_override if reduction_override else self.reduction)
-        loss = self.loss_weight * contrastive_loss(
-            pred, target, weight, reduction=reduction, avg_factor=avg_factor)
-        return loss
+    def forward(self, feats, mode='train'):
+        # Calculate cosine similarity
+        cos_sim = nn.functional.cosine_similarity(feats[:,None,:], feats[None,:,:], dim=-1)
+        # Mask out cosine similarity to itself
+        self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
+        cos_sim.masked_fill_(self_mask, -9e15)
+        # Find positive example -> batch_size//2 away from the original example
+        pos_mask = self_mask.roll(shifts=cos_sim.shape[0]//2, dims=0)
+        # InfoNCE loss
+        cos_sim = cos_sim / self.temperature
+        nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+        nll = nll.mean()
+
+        # Logging loss
+        self.log(mode+'_loss', nll)
+        # Get ranking position of positive example
+        comb_sim = torch.cat([cos_sim[pos_mask][:,None],  # First position positive example
+                              cos_sim.masked_fill(pos_mask, -9e15)],
+                             dim=-1)
+        sim_argsort = comb_sim.argsort(dim=-1, descending=True).argmin(dim=-1)
+        # Logging ranking metrics
+        self.log(mode+'_acc_top1', (sim_argsort == 0).float().mean())
+        self.log(mode+'_acc_top5', (sim_argsort < 5).float().mean())
+        self.log(mode+'_acc_mean_pos', 1+sim_argsort.float().mean())
+
+        return nll
+
+        
