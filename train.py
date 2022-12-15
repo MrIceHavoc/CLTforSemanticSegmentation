1e957d0dd30da86c6c30c280dc820807fde46d6d
MrIceHavoc <afonso.fernandes100@gmail.com>
2022-12-15 23:48:43 +0900

    Partial impl of decode head

--- train.py
+++ train.py
@@ -6,9 +6,6 @@ import torch.nn as nn
 
 from torch.nn.init import trunc_normal_
 
-from vision_transformer import VisionTransformer
-from swin_transformer import SwinTransformer
-
 import logging
 import os
 import sys
@@ -47,34 +44,110 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 from transformers import EarlyStoppingCallback, IntervalStrategy
-from transformers import SwinConfig, SwinModel, AutoImageProcessor, SegformerForSemanticSegmentation, SegformerDecodeHead, SegformerConfig, SegformerModel, Trainer, SegformerFeatureExtractor
+from transformers import SwinConfig, SwinModel, SegformerPreTrainedModel, SegformerForSemanticSegmentation, SegformerConfig, SegformerModel, Trainer, SegformerFeatureExtractor
 from datasets import load_dataset
 from transformers import TrainingArguments
 
 from transformers import AutoModel
+
+
+class SegformerMLP(nn.Module):
+    """
+    Linear Embedding.
+    """
+
+    def __init__(self, config: SegformerConfig, input_dim):
+        super().__init__()
+        self.proj = nn.Linear(input_dim, config.decoder_hidden_size)
+
+    def forward(self, hidden_states: torch.Tensor):
+        hidden_states = hidden_states.flatten(2).transpose(1, 2)
+        hidden_states = self.proj(hidden_states)
+        return hidden_states
+
+
+class SegformerDecodeHead(SegformerPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        # linear layers which will unify the channel dimension of each of the encoder blocks to the same config.decoder_hidden_size
+        mlps = []
+        for i in range(config.num_encoder_blocks):
+            mlp = SegformerMLP(config, input_dim=config.hidden_sizes[i])
+            mlps.append(mlp)
+        self.linear_c = nn.ModuleList(mlps)
+
+        # the following 3 layers implement the ConvModule of the original implementation
+        self.linear_fuse = nn.Conv2d(
+            in_channels=config.decoder_hidden_size * config.num_encoder_blocks,
+            out_channels=config.decoder_hidden_size,
+            kernel_size=1,
+            bias=False,
+        )
+        self.batch_norm = nn.BatchNorm2d(config.decoder_hidden_size)
+        self.activation = nn.ReLU()
+
+        self.dropout = nn.Dropout(config.classifier_dropout_prob)
+        self.classifier = nn.Conv2d(config.decoder_hidden_size, config.num_labels, kernel_size=1)
+
+        self.config = config
+
+    def forward(self, encoder_hidden_states: torch.FloatTensor) -> torch.Tensor:
+        batch_size = encoder_hidden_states[-1].shape[0]
+
+        all_hidden_states = ()
+        for encoder_hidden_state, mlp in zip(encoder_hidden_states, self.linear_c):
+            #if self.config.reshape_last_stage is False and encoder_hidden_state.ndim == 3:
+            height = width = int(math.sqrt(encoder_hidden_state.shape[-1]))
+            encoder_hidden_state = (
+                encoder_hidden_state.reshape(batch_size, height, width, -1).permute(0, 3, 1, 2).contiguous()
+            )
+
+            print("Enc hidden", encoder_hidden_state.shape)
+
+            # unify channel dimension
+            height, width = encoder_hidden_state.shape[2], encoder_hidden_state.shape[3]
+            encoder_hidden_state = mlp(encoder_hidden_state)
+            encoder_hidden_state = encoder_hidden_state.permute(0, 2, 1)
+            encoder_hidden_state = encoder_hidden_state.reshape(batch_size, -1, height, width)
+            # upsample
+            encoder_hidden_state = nn.functional.interpolate(
+                encoder_hidden_state, size=encoder_hidden_states[0].size()[2:], mode="bilinear", align_corners=False
+            )
+            all_hidden_states += (encoder_hidden_state,)
+
+        hidden_states = self.linear_fuse(torch.cat(all_hidden_states[::-1], dim=1))
+        hidden_states = self.batch_norm(hidden_states)
+        hidden_states = self.activation(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+
+        # logits are of shape (batch_size, num_labels, height/4, width/4)
+        logits = self.classifier(hidden_states)
+
+        return logits
+        
+
 class PosModel(nn.Module):
     def __init__(self):
         super(PosModel, self).__init__()
 
         self.base_model = SwinModel.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
-        #(dense2): Linear(in_features=1024, out_features=256, bias=True)
-        #      (dropout): Dropout(p=0.0, inplace=False)
         self.linear = nn.Linear(768, 256) # output features from bert is 768 and 2 is ur number of labels
-        self.linear2 = nn.Linear(256, 128)
-        self.linear3 = nn.Linear(128, 32)
-        self.dropout = nn.Dropout(0.2)
+        #self.linear2 = nn.Linear(256, 128)
+        #self.linear3 = nn.Linear(128, 32)
+        self.proj = nn.Conv2d(2, 256, kernel_size=1, stride=1, padding=0)
         self.decode_head = SegformerDecodeHead(SegformerConfig())
-        self.decode_head.classifier = nn.Conv2d(256, 21, kernel_size=(1, 1), stride=(1, 1))
 
     def forward(self, pixel_values, labels):#attn_mask
-        print("!!!!!!!!!!!!",self.base_model(pixel_values).last_hidden_state.shape)
+        batch_size = pixel_values.shape[0]
         outputs = self.base_model(pixel_values).last_hidden_state#, attention_mask=attn_mask
-        print(outputs)
         # You write you new head here
         outputs = self.linear(outputs)
-        outputs = self.linear2(outputs)
-        outputs = self.linear3(outputs)
-        outputs = self.dropout(outputs[0])
+        #outputs = self.linear2(outputs)
+        #outputs = self.linear3(outputs)
+        outputs = self.proj(outputs)
+        outputs = outputs.reshape(batch_size, outputs.shape[0], outputs.shape[1], -1).permute(0, 3, 1, 2).contiguous()
+        print(outputs.shape)
+        #outputs = outputs.reshape(batch_size, height, width, -1).permute(0, 3, 1, 2).contiguous()
         outputs = self.decode_head(outputs)
 
         return outputs
